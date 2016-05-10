3f3ddec5b8e01dd54728026b390211ac5a60689a
Greg Walker <michael.walker@gsa.gov>
2016-05-10 10:49:39 -0500

    Don't pull in env.js for coverage

--- tests/coverage.js
+++ tests/coverage.js
@@ -3,6 +3,10 @@
 // require() all the things, so they get included
 // in the coverage reports.
 
-require('../env');
+process.env.TRELLO_API_KEY = 'trello-api-key';
+process.env.TRELLO_API_TOK = 'trello-api-tok';
+
 require('../actions');
 require('../webhookHandlers');
+
+require('tap').pass('Coverage initialized');
