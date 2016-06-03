ca150f611da732bb571cb941ec55ffe6dd9c360f
Greg Walker <michael.walker@gsa.gov>
2016-06-03 11:02:49 -0500

    These bits of tests need to come out as well

--- test/webhookHandlers/intake.js
+++ test/webhookHandlers/intake.js
@@ -7,10 +7,8 @@ const mockRequire = require('mock-require');
 
 const sandbox = sinon.sandbox.create();
 const iaaGo = sandbox.mock();
-const iaaCompleted = sandbox.mock();
 
 mockRequire('../../webhookHandlers/intake/move-to-iaa-go', iaaGo);
-mockRequire('../../webhookHandlers/intake/move-to-iaa-completed', iaaCompleted);
 
 // Disable logging to the console.
 require('@erdc-itl/simple-logger').setOptions({ console: false });
@@ -36,8 +34,6 @@ tap.test('webhook handlers - intake', t1 => {
         test.pass('resolves');
         test.equal(iaaGo.callCount, 1, 'calls IAA Go handler one time');
         test.equal(iaaGo.args[0][0], eventObj, 'passes the event to IAA Go handler');
-        test.equal(iaaCompleted.callCount, 1, 'calls IAA Completed handler one time');
-        test.equal(iaaCompleted.args[0][0], eventObj, 'passes the event to IAA Completed handler');
       })
       .catch(() => {
         test.fail('resolves');
@@ -45,27 +41,13 @@ tap.test('webhook handlers - intake', t1 => {
       .then(test.done);
   };
 
-  t1.test('IAA Go and IAA Completed handlers reject', t2 => {
+  t1.test('IAA Go handler rejects', t2 => {
     iaaGo.rejects(err);
-    iaaCompleted.rejects(err);
     actualTest(t2);
   });
 
-  t1.test('IAA Go handler rejects and IAA Completed handler resolves', t2 => {
-    iaaGo.rejects(err);
-    iaaCompleted.resolves();
-    actualTest(t2);
-  });
-
-  t1.test('IAA Go handler resolves and IAA Completed handler rejects', t2 => {
+  t1.test('IAA Go handler resolves', t2 => {
     iaaGo.resolves();
-    iaaCompleted.rejects(err);
-    actualTest(t2);
-  });
-
-  t1.test('IAA Go and IAA Completed handlers resolve', t2 => {
-    iaaGo.resolves();
-    iaaCompleted.resolves();
     actualTest(t2);
   });
 
--- webhookHandlers/intake.js
+++ webhookHandlers/intake.js
@@ -3,8 +3,6 @@
 const iaaGo = require('./intake/move-to-iaa-go')
 
 module.exports = function executeIntakeHandlers(e) {
-  const func = fn => (() => fn(e));
-
   return iaaGo(e)
     .catch(() => null);
 }
