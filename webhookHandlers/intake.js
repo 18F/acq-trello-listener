17e0067ab243bf766574df8df77248684dd95dbe
Greg Walker <michael.walker@gsa.gov>
2016-06-03 10:57:50 -0500

    Add bones of IAA completed handling

--- webhookHandlers/intake.js
+++ webhookHandlers/intake.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const iaaGo = require('./intake/move-to-iaa-go')
+const iaaCompleted = require('./intake/move-to-iaa-completed');
 
 module.exports = function executeIntakeHandlers(e) {
   const func = fn => (() => fn(e));
 
   return iaaGo(e)
+    .then(func(iaaCompleted), func(iaaCompleted))
     .catch(() => null);
 }
