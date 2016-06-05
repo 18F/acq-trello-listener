f1e7aabd7c89726d0840f53b2e5a6cbec9a5aa57
Greg Walker <michael.walker@gsa.gov>
2016-06-05 18:10:06 -0500

    Skeleton

--- webhookHandlers/intake/move-to-iaa-go.js
+++ webhookHandlers/intake/move-to-iaa-go.js
@@ -5,12 +5,14 @@ const actions = require('../../actions');
 const eventTypes = require('../event-types');
 const log = require('../../logger')('intake handler: IAA Go');
 
-const createBPAComponents = require('./move-to-iaa-go/create-bpa-components');
+const createChecklists = require('./move-to-iaa-go/create-checklists-on-intake-card');
 const createATCCard = require('./move-to-iaa-go/create-atc-card');
+const createBPAComponents = require('./move-to-iaa-go/create-bpa-components');
 
 module.exports = function handleIntakeWebhookEvent(e) {
   if (eventTypes(e) === eventTypes.CardMoved && e.action.data.listAfter.name.startsWith('IAA Go')) {
-    return createATCCard(e)
+    return createChecklists(e)
+      .then(() => createATCCard(e))
       .then(() => createBPAComponents(e))
   } else {
     return Promise.reject(new Error('Not a move to IAA Go'));
---
+++ webhookHandlers/intake/move-to-iaa-go/create-checklists-on-intake-card.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const trello = require('../../../trello');
+const actions = require('../../../actions');
+
+module.exports = function createChecklists(e) {
+  return Promise.resolve();
+};
