0ac904137c32b3feb02a6c2ebda2c32815b875b6
Greg Walker <michael.walker@gsa.gov>
2016-06-06 08:15:46 -0500

    The actual functionality

--- webhookHandlers/intake.js
+++ webhookHandlers/intake.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const iaaGo = require('./intake/move-to-iaa-go')
-const iaaCompleted = require('./intake/move-to-iaa-completed');
 
 module.exports = function executeIntakeHandlers(e) {
+  // Wraps functions in a 0-argument shell and captures
+  // the event variable in a closure.  This way the
+  // wrapped functions can be passed directly into
+  // then/catch.
   const func = fn => (() => fn(e));
+  
+  const iaaCompleted = func(require('./intake/move-to-iaa-completed'));
 
   return iaaGo(e)
-    .then(func(iaaCompleted), func(iaaCompleted))
+    .then(iaaCompleted, iaaCompleted)
     .catch(() => null);
 }
--- webhookHandlers/intake/move-to-iaa-completed.js
+++ webhookHandlers/intake/move-to-iaa-completed.js
@@ -6,34 +6,34 @@ const eventTypes = require('../event-types');
 const log = require('../../logger')('intake handler: IAA Completed');
 
 const bpaStartsWith = 'Agile BPA';
-const iaaCompleteStartsWith = 'IAA Completed Work Begins';
+const iaaCompleteStartsWith = 'IAA Completed Work Begin';
+
+const bpaURLRegex = /(^|\n)### Agile BPA Links\n\n.*\n\* \[BPA Dashboard\]\((http.*?)\)(\n|$)/;
+const bpaCardIDRegex = /https:\/\/trello\.com\/c\/([^\/]+)\/.+/;
 
 module.exports = function handleIntakeWebhookEvent(e) {
-  return new Promise((resolve, reject) => {
-    if (eventTypes(e) === eventTypes.CardMoved) {
-      if (e.action.data.card.name.startsWith(bpaStartsWith) && e.action.data.listAfter.name.startsWith(iaaCompleteStartsWith)) {
-        // It's possible this card was moved back into the
-        // IAA Go list from a later list and already has
-        // an associated BPA card and board.  So, we need
-        // to check the description.
-        trello.get(`/1/cards/${e.action.data.card.id}`, (err, card) => {
-          if (err) {
-            return reject(err);
-          }
+  if(eventTypes(e) !== eventTypes.CardMoved) {
+    log.verbose('Not a card move');
+    return Promise.reject('Not a card move');
+  }
 
-          if (!card.desc.match(/(^|\n)\*\*Agile BPA Links\*\*\n\n.*\n\* BPA Dashboard: (http.*?)(\n|$)/)) {
-            return reject('Intake BPA does not have an associated BPA Dashboard card');
-          }
+  if (!e.action.data.card.name.startsWith(bpaStartsWith) || !e.action.data.listAfter.name.startsWith(iaaCompleteStartsWith)) {
+    log.verbose(`Not an Agile BPA card, or not a move into ${iaaCompleteStartsWith}`);
+    return Promise.reject(`Not an Agile  BPA card, or not a move into ${iaaCompleteStartsWith}`);
+  }
 
-          bpaCardURL = card.desc.match(/(^|\n)\*\*Agile BPA Links\*\*\n\n.*\n\* BPA Dashboard: (http.*?)(\n|$)/)[2];
-
-
-        });
-      } else {
-        reject('Not an Agile  BPA card, or not a move into IAA Go');
+  return trello.get(`/1/cards/${e.action.data.card.id}`) //, (err, card) => {
+    .then(card => {
+      // Make sure this intake card has an associated BPA Dashboard card
+      if (!card.desc.match(bpaURLRegex)) {
+        log.info('Intake BPA does not have an associated BPA Dashboard card');
+        throw new Error('Intake BPA does not have an associated BPA Dashboard card');
       }
-    } else {
-      reject('Not a card move');
-    }
-  });
+
+      const bpaCardURL = card.desc.match(bpaURLRegex)[2];
+      const bpaCardID = bpaCardURL.match(bpaCardIDRegex)[1];
+
+      log.info(`Intake card '${card.name}' moved to '${iaaCompleteStartsWith}'. Moving associated BPA Dashboard card.`);
+      return trello.put(`/1/cards/${bpaCardID}/idList`, { value: process.env.TRELLO_BPA_WORKSHOP_PREP_LIST_ID });
+    });
 };
