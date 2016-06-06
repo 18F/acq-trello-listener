fc8b1d9aa2c45fa03f6cbe0a972946a83eeee25d
Greg Walker <michael.walker@gsa.gov>
2016-06-05 19:06:22 -0500

    Call the action correctly

--- webhookHandlers/intake/move-to-iaa-go/create-checklists-on-intake-card.js
+++ webhookHandlers/intake/move-to-iaa-go/create-checklists-on-intake-card.js
@@ -4,5 +4,5 @@ const trello = require('../../../trello');
 const actions = require('../../../actions');
 
 module.exports = function createChecklists(e) {
-  return Promise.resolve();
+  return actions.addIntakeChecklist(e.action.data.card.id);
 };
