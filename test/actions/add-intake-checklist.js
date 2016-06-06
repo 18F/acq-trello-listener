a6338d4ecb1c222dfed6405ebbbae15fbd7027f2
Greg Walker <michael.walker@gsa.gov>
2016-06-06 14:35:21 -0500

    Make test piece names meaningful instead of t1/t2/etc.

--- test/actions/add-intake-checklist.js
+++ test/actions/add-intake-checklist.js
@@ -18,49 +18,49 @@ const intakeCardID = 'intake-card-id';
 // Disable logging to the console.
 require('@erdc-itl/simple-logger').setOptions({ console: false });
 
-tap.test('actions - add intake checklist', t1 => {
-  t1.beforeEach(done => {
-    sandbox.reset();
-    done();
-  });
+tap.beforeEach(done => {
+  sandbox.reset();
+  done();
+});
 
-  t1.teardown(() => {
-    sandbox.restore();
-  });
+tap.teardown(() => {
+  sandbox.restore();
+});
 
-  t1.test('trello get throws error (fetching existing checklists)', t2 => {
+tap.test('actions - add intake checklist', trelloGetTest => {
+  trelloGetTest.test('trello get throws error (fetching existing checklists)', test => {
     const err = new Error('Test error');
     trelloGet.rejects(err);
 
     addIntakeChecklist(intakeCardID)
       .then(() => {
-        t2.fail('rejects');
+        test.fail('rejects');
       })
       .catch(e => {
-        t2.pass('rejects');
-        t2.equals(trelloGet.callCount, 1, 'trello get called one time');
-        t2.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
-        t2.equals(trelloPost.callCount, 0, 'trello post called 0 times');
-        t2.equals(e, err, 'returns the expected error');
+        test.pass('rejects');
+        test.equals(trelloGet.callCount, 1, 'trello get called one time');
+        test.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
+        test.equals(trelloPost.callCount, 0, 'trello post called 0 times');
+        test.equals(e, err, 'returns the expected error');
       })
-      .then(t2.done);
+      .then(test.done);
   });
 
-  t1.test('trello get returns a list of checklists include intake forms', t2 => {
+  trelloGetTest.test('trello get returns a list of checklists include intake forms', test => {
     trelloGet.resolves([{ name: 'Intake Forms' }]);
 
     addIntakeChecklist(intakeCardID)
       .then(() => {
-        t2.fail('rejects');
+        test.fail('rejects');
       })
       .catch(e => {
-        t2.pass('rejects');
-        t2.equals(trelloGet.callCount, 1, 'trello get called one time');
-        t2.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
-        t2.equals(trelloPost.callCount, 0, 'trello post called 0 times');
-        t2.equals(e.message, 'Intake card already has an intake forms checklist', 'returns the expected error');
+        test.pass('rejects');
+        test.equals(trelloGet.callCount, 1, 'trello get called one time');
+        test.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
+        test.equals(trelloPost.callCount, 0, 'trello post called 0 times');
+        test.equals(e.message, 'Intake card already has an intake forms checklist', 'returns the expected error');
       })
-      .then(t2.done);
+      .then(test.done);
   });
 
   const getPassValues = [
@@ -69,37 +69,37 @@ tap.test('actions - add intake checklist', t1 => {
   ];
 
   getPassValues.forEach(getPass => {
-    t1.test(`trello get returns ${getPass.name}`, t2 => {
+    trelloGetTest.test(`trello get returns ${getPass.name}`, trelloPostChecklistTest => {
       const expectedChecklistReq = {
         idCard: intakeCardID,
         name: 'Intake Forms'
       };
 
-      t2.test('trello post (create checklist) rejects', t3 => {
+      trelloPostChecklistTest.test('trello post (create checklist) rejects', test => {
         const err = new Error('Test error');
         trelloGet.resolves(getPass.value);
         trelloPost.onCall(0).rejects(err);
 
         addIntakeChecklist(intakeCardID)
           .then(() => {
-            t3.fail('rejects');
+            test.fail('rejects');
           })
           .catch(e => {
-            t3.pass('rejects');
-            t3.equals(trelloGet.callCount, 1, 'trello get called one time');
-            t3.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
-            t3.equals(trelloPost.callCount, 1, 'trello post called one time');
-            t3.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
-            t3.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
-            t3.equals(e, err, 'returns the expected error');
+            test.pass('rejects');
+            test.equals(trelloGet.callCount, 1, 'trello get called one time');
+            test.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
+            test.equals(trelloPost.callCount, 1, 'trello post called one time');
+            test.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
+            test.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
+            test.equals(e, err, 'returns the expected error');
           })
-          .then(t3.done);
+          .then(test.done);
       });
 
-      t2.test('trello post (create checklist) resolves', t3 => {
+      trelloPostChecklistTest.test('trello post (create checklist) resolves', trelloPostFirstCheckItemTest => {
         const checklistID = 'checklist-id';
 
-        t3.test('trello post (add first item) rejects', t4 => {
+        trelloPostFirstCheckItemTest.test('trello post (add first item) rejects', test => {
           const err = new Error('Test error');
           trelloGet.resolves(getPass.value);
           trelloPost.onCall(0).resolves({ id: checklistID });
@@ -107,24 +107,24 @@ tap.test('actions - add intake checklist', t1 => {
 
           addIntakeChecklist(intakeCardID)
             .then(() => {
-              t4.fail('rejects');
+              test.fail('rejects');
             })
             .catch(e => {
-              t4.pass('rejects');
-              t4.equals(trelloGet.callCount, 1, 'trello get called one time');
-              t4.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
-              t4.equals(trelloPost.callCount, 2, 'trello post called two times');
-              t4.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
-              t4.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
-              t4.equals(trelloPost.args[1][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
-              t4.same(trelloPost.args[1][1], { name: '7600 SOW' }, 'sends the expected arguments');
-              t4.equals(e, err, 'returns the expected error');
+              test.pass('rejects');
+              test.equals(trelloGet.callCount, 1, 'trello get called one time');
+              test.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
+              test.equals(trelloPost.callCount, 2, 'trello post called two times');
+              test.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
+              test.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
+              test.equals(trelloPost.args[1][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
+              test.same(trelloPost.args[1][1], { name: '7600 SOW' }, 'sends the expected arguments');
+              test.equals(e, err, 'returns the expected error');
             })
-            .then(t4.done);
+            .then(test.done);
         });
 
-        t3.test('trello post (add first item) resolves', t4 => {
-          t4.test('trello post (add second item) rejects', t5 => {
+        trelloPostFirstCheckItemTest.test('trello post (add first item) resolves', trelloPostSecondCheckItemTest => {
+          trelloPostSecondCheckItemTest.test('trello post (add second item) rejects', test => {
             const err = new Error('Test error');
             trelloGet.resolves(getPass.value);
             trelloPost.onCall(0).resolves({ id: checklistID });
@@ -133,25 +133,25 @@ tap.test('actions - add intake checklist', t1 => {
 
             addIntakeChecklist(intakeCardID)
               .then(() => {
-                t5.fail('rejects');
+                test.fail('rejects');
               })
               .catch(e => {
-                t5.pass('rejects');
-                t5.equals(trelloGet.callCount, 1, 'trello get called one time');
-                t5.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
-                t5.equals(trelloPost.callCount, 3, 'trello post called three times');
-                t5.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
-                t5.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
-                t5.equals(trelloPost.args[1][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
-                t5.same(trelloPost.args[1][1], { name: '7600 SOW' }, 'sends the expected arguments');
-                t5.equals(trelloPost.args[2][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
-                t5.same(trelloPost.args[2][1], { name: 'Budget Estimate' }, 'sends the expected arguments');
-                t5.equals(e, err, 'returns the expected error');
+                test.pass('rejects');
+                test.equals(trelloGet.callCount, 1, 'trello get called one time');
+                test.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
+                test.equals(trelloPost.callCount, 3, 'trello post called three times');
+                test.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
+                test.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
+                test.equals(trelloPost.args[1][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
+                test.same(trelloPost.args[1][1], { name: '7600 SOW' }, 'sends the expected arguments');
+                test.equals(trelloPost.args[2][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
+                test.same(trelloPost.args[2][1], { name: 'Budget Estimate' }, 'sends the expected arguments');
+                test.equals(e, err, 'returns the expected error');
               })
-              .then(t5.done);
+              .then(test.done);
           });
 
-          t4.test('trello post (add second item) resolves', t5 => {
+          trelloPostSecondCheckItemTest.test('trello post (add second item) resolves', test => {
             const err = new Error('Test error');
             trelloGet.resolves(getPass.value);
             trelloPost.onCall(0).resolves({ id: checklistID });
@@ -160,32 +160,32 @@ tap.test('actions - add intake checklist', t1 => {
 
             addIntakeChecklist(intakeCardID)
               .then(() => {
-                t5.pass('resolves');
-                t5.equals(trelloGet.callCount, 1, 'trello get called one time');
-                t5.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
-                t5.equals(trelloPost.callCount, 3, 'trello post called three times');
-                t5.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
-                t5.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
-                t5.equals(trelloPost.args[1][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
-                t5.same(trelloPost.args[1][1], { name: '7600 SOW' }, 'sends the expected arguments');
-                t5.equals(trelloPost.args[2][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
-                t5.same(trelloPost.args[2][1], { name: 'Budget Estimate' }, 'sends the expected arguments');
+                test.pass('resolves');
+                test.equals(trelloGet.callCount, 1, 'trello get called one time');
+                test.equals(trelloGet.args[0][0], `/1/cards/${intakeCardID}/checklists`, 'calls the expected URL');
+                test.equals(trelloPost.callCount, 3, 'trello post called three times');
+                test.equals(trelloPost.args[0][0], `/1/checklists`, 'calls the expected URL');
+                test.same(trelloPost.args[0][1], expectedChecklistReq, 'sends the expected arguments');
+                test.equals(trelloPost.args[1][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
+                test.same(trelloPost.args[1][1], { name: '7600 SOW' }, 'sends the expected arguments');
+                test.equals(trelloPost.args[2][0], `/1/checklists/${checklistID}/checkItems`, 'calls the expected URL');
+                test.same(trelloPost.args[2][1], { name: 'Budget Estimate' }, 'sends the expected arguments');
               })
               .catch(e => {
-                t5.fail('resolves');
+                test.fail('resolves');
               })
-              .then(t5.done);
+              .then(test.done);
           });
 
-          t4.done();
+          trelloPostSecondCheckItemTest.done();
         });
 
-        t3.done();
+        trelloPostFirstCheckItemTest.done();
       });
 
-      t2.done();
+      trelloPostChecklistTest.done();
     });
   });
 
-  t1.done();
+  trelloGetTest.done();
 });
